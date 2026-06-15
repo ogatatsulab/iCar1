00cae2368f84a78e74b28cb85a16c4328a818318
user <user@host.docker.internal>
2026-06-15 20:26:24 +0900

    (1)LEDの点灯時間を0.5秒に変更する。

--- src/User.cpp
+++ src/User.cpp
@@ -6,7 +6,7 @@ void setup() {
 void loop() {
     while (1) {
         digitalWrite(PIN_LED1, HIGH);
-        delay(1000);
+        delay(500);
         digitalWrite(PIN_LED1, LOW);
         delay(1000);
     }
